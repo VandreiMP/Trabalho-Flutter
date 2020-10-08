7bcf9cacc06fdcc4c9b3c1d01135b10a9d421f98
Vandrei Masiero Padilha <vandrei.masiero.padilha@gmail.com>
2020-10-07 23:13:43 -0300

    terceiro commit

--- lib/BarraFerramentas/Bloc/barra-ferramentas-bloc.dart
+++ lib/BarraFerramentas/Bloc/barra-ferramentas-bloc.dart
@@ -2,7 +2,7 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:controle_romaneio/Entidades/Bloc/romaneio-bloc.dart';
 import 'package:flutter/material.dart';
 class BarraFerramentasBloc extends BlocBase {
-  final BuildContext contextoAplicacao;
+  final BuildContext contextoAplicacao; 
   final String chaveConsulta;
 
   BarraFerramentasBloc(this.contextoAplicacao, this.chaveConsulta);
